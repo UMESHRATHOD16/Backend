6b9a34bff2c0d2d74012041785acb5e840a83767
UMESHRATHOD16 <umeshhhhhrathod@gmail.com>
2026-07-09 23:09:49 +0530

    commit:just a commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,24 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req,res)=> {
+//  register user process : 
+//  creating a new account so : first mail, name and password
+//  or signup via google 
+// my model has : userName, emai, password, fullName, avatar, coverPage 
+// validation - info is not empty smhw
+// check if user already exists: check userName and email
+// files - avatar and coverImage
+// upload them to cloudinary, avatar in backend received or not
+// then create entry in db
+// 
+
     res.status(200).json({
         message: "ok"
     })
+
+const {fullName, email, username, password} = req.body
+console.log(`email is ${email}`)
+
 })
 
 export {registerUser}
