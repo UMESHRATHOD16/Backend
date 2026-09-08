e49cf807e65cf517355be207bf2d3477b2f6b0d8
UMESHRATHOD16 <umeshhhhhrathod@gmail.com>
2026-09-08 22:43:58 +0530

    feat: added login feature,(no route yet)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,27 @@
+// STRICTLY COMMENTS ARE USEFULL
+
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.models.js"
 import {uploadOnCloudinary} from "../utils/Cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens = async(userId) =>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return {accessToken, refreshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
 
 const registerUser = asyncHandler(async (req,res)=> {
 //  register user process : 
@@ -93,4 +111,69 @@ console.log(`username is ${username}`)
     )
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async (req,res) => {
+    // todos for login
+    // req body -> data
+    // check if user exists, userName or email
+    // if exists then check password
+    // access token and refresh token
+    // send AT and RT using cookies (access token and refresh token)
+
+
+    const {email,username,password} = req.body  // req body data done
+
+    if(!username || !email){
+        throw new ApiError(400,"Username or password is required")
+    }
+
+    const user = await User.findOne({    // checked if username or email existed in DB or not
+        $or: [ { username }, {email} ]          // This typa shit it gold, remember this 
+    })
+
+    if(!user){
+        throw new ApiError(404,"User does not exists please register")  // this is easy underStandable right !!
+    }
+
+    // now check Password :
+    const isPasswordValid = await user.isPasswordCorrect(password)   
+
+    if(!isPasswordValid){
+        throw new ApiError(401,"Invalid User Credentials")
+    }
+
+    // The fucking literally dont get it thing (its undestandable but idk the significance)
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+    // the upper Line of code may become expensive while users increase so take care of it
+    // I need to underStand my codeBase again brathaaa....
+
+    // Cookies Timeee (biscuuuttsss)
+
+    const options = {
+        httpOnly : true,
+        secure : true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToke",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User LoggedIn Successfully"
+        )
+    )
+
+
+
+})
+
+export {
+    registerUser,
+    loginUser
+}
